85dba5d79bc0cc5c979de44802fb8c27bf88d87d
sopiko224 <sophiepkhikidze@gmail.com>
2020-10-16 20:29:05 +0400

    add new things

---
+++ script.js
@@ -0,0 +1,10 @@
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+      document.getElementById("myOverlay").style.display = "block";
+      console.log("mySidebar");
+}
+
+function w3_close() {
+      document.getElementById("mySidebar").style.display = "none";
+      document.getElementById("myOverlay").style.display = "none";
+}
